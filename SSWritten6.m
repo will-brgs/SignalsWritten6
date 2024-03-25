53f48534ba7a87ba094641c342204e56c1b2e768
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-03-25 16:08:56 -0500

    bwrve

---
+++ SSWritten6.m
@@ -0,0 +1,9 @@
+%% Signals and Systems Matlab Homework #6
+%% Introduction
+% * Author:                   Will Burgess
+% * Class:                    ESE 351
+% * Date:                     Created 3/25/2024, Last Edited 4/02/2024
+% * With contributions from:  Mack Larosa, Tasha Igic, Mischa Tranor
+%% 1.d.i
+%% 1.d.ii
+%% 1.d.iii
